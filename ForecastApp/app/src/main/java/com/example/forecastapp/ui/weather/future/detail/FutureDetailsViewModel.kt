ba5384a1c9334b8ad71580c8cde2ff31031ed0e5
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2019-06-01 03:25:52 +0600

    Fragment setup

---
+++ ForecastApp/app/src/main/java/com/example/forecastapp/ui/weather/future/detail/FutureDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.forecastapp.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel;
+
+class FutureDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
